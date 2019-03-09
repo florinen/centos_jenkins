622a728d35b3b486bb1b3c6bc21ace657c25a599
Farkhod <sadykovfarkhod@gmail.com>
2019-03-09 17:44:49 -0600

    Added stage testing

--- Jenkins/jenkins.groovy
+++ Jenkins/jenkins.groovy
@@ -2,26 +2,26 @@ import hudson.EnvVars
 
 def app
 
-
 node {
 
+  // Pooling the docker image
   checkout scm
-
-  stage('New release GIT') {
-
-    // Get latest release from local git
-    env.build = sh returnStdout: true, script: 'git describe --abbrev=0 --tags'
-  }
 
   stage('Build docker image') {
 
-    // Build the image
+      // Build the docker image
       app = docker.build("fsadykov/centos_jenkins", "-f ${WORKSPACE}/Dockerfile/Dockerfile .")
+  }
+
+  stage('Testing the aplication') {
+    app.inside {
+      sh 'ls /opt/run.sh'
+    }  
   }
 
   stage('Push image') {
 
-     // Push image to the Nexus with new release
+     // Push docker image to the Docker hub
       docker.withRegistry('', 'dockerhub-credentials') {
           app.push("0.${BUILD_NUMBER}")
           app.push("latest")
