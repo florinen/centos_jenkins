1ae5f2ecf1c8284e1251d589eb178d45cc1a07f8
Farkhod <sadykovfarkhod@gmail.com>
2019-03-09 16:58:46 -0600

    Added Build number

--- Jenkins/jenkins.groovy
+++ Jenkins/jenkins.groovy
@@ -23,7 +23,7 @@ node {
 
      // Push image to the Nexus with new release
       docker.withRegistry('', 'dockerhub-credentials') {
-          app.push("${env.build}")
+          app.push("${BUILD_NUMBER}")
           app.push("latest")
       }
   }
