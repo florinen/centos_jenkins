51cad32d200bc85126a83437372a5c8adf715c70
Farkhod <sadykovfarkhod@gmail.com>
2019-03-09 16:07:36 -0600

    Checking for Jenkins build

---
+++ Jenkins/jenkins.groovy
@@ -0,0 +1,30 @@
+mport hudson.EnvVars
+
+def app
+
+
+node {
+
+  checkout scm
+
+  stage('New release GIT') {
+
+    // Get latest release from local git
+    env.build = sh returnStdout: true, script: 'git describe --abbrev=0 --tags'
+  }
+
+  stage('Build docker image') {
+
+    // Build the image
+      app = docker.build("fscoding", "-f ${WORKSPACE}/Dockerfile/Dockerfile .")
+  }
+
+  stage('Push image') {
+
+     // Push image to the Nexus with new release
+      docker.withRegistry('docker.io/fsadykov/centos_jenkins', 'dockerhub-credentials') {
+          app.push("${env.build}")
+          app.push("latest")
+      }
+  }
+}
