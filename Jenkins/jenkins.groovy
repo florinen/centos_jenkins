ca8d08f2b628266e39af54ff820f516155ebfe9c
Farkhod <sadykovfarkhod@gmail.com>
2019-05-25 23:44:27 -0500

    Fixed docker push

--- Jenkins/jenkins.groovy
+++ Jenkins/jenkins.groovy
@@ -6,8 +6,7 @@ def app
 properties([
   parameters([
     booleanParam(defaultValue: false,
-      description: 'Click this if you would like to deploy to latest',
-      name: 'PUSH_LATEST'
+      description: 'Click this if you would like to deploy to latest', name: 'PUSH_LATEST'
       )])])
 
 
@@ -27,10 +26,11 @@ node {
 
      // Push docker image to the Docker hub
       docker.withRegistry('', 'dockerhub-credentials') {
-          app.push("0.${BUILD_NUMBER}")
+          app.push("jenkins-kube.${BUILD_NUMBER}")
+          
           // If push to latest parameters selected
           if (params.PUSH_LATEST){
-            app.push("latest")
+            app.push("jenkins-kube")
           }
       }
   }
