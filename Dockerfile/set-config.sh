7933fc170d20ca6fd031febb7fc04f4116aca220
Farkhod <sadykovfarkhod@gmail.com>
2019-05-31 16:01:04 -0500

    Deleted logging

--- Dockerfile/set-config.sh
+++ Dockerfile/set-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl config set-cluster fuchicorp-cluster --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt  >> /var/log/kube-set-config.log
-kubectl config set-context fuchicorp-context --cluster=fuchicorp-cluster >> /var/log/kube-set-config.log
-kubectl config set-credentials fuchicorp-user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" >> /var/log/kube-set-config.log
-kubectl config set-context fuchicorp-context --user=fuchicorp-user >> /var/log/kube-set-config.log
-kubectl config use-context fuchicorp-context >> /var/log/kube-set-config.log
+kubectl config set-cluster fuchicorp-cluster --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt  
+kubectl config set-context fuchicorp-context --cluster=fuchicorp-cluster
+kubectl config set-credentials fuchicorp-user --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+kubectl config set-context fuchicorp-context --user=fuchicorp-user
+kubectl config use-context fuchicorp-context
